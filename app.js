0d760e0b7ad5b2dab352c50d984766d3c9a3d010
David Vallecios <00078217@uca.edu.sv>
2021-11-15 17:02:47 -0600

    Arreglando problemas de CORS

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ database.connect();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  );
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
